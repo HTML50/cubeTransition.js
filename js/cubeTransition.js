c4bdc3c41f167a8090f1a961754da3f3ab50b1ab
2byte <iambenf@gmail.com>
2016-10-08 16:26:54 +0800

    1.0
    mobile support, others fine

---
+++ js/cubeTransition.js
@@ -0,0 +1,98 @@
+(function($) {
+
+	var length = $('#cubeTransition>div').length,
+		current = 1,
+		next = 1,
+		outClass, inClass, onGoing = false;
+	$('#cubeTransition>div:eq(' + (current - 1) + ')').show()
+
+
+	for (i = 0; i < length; i++) {
+		var bullet = $("<li></li>");
+		if (i == 0) bullet.addClass('active');
+		$("#bullets").append(bullet);
+	}
+
+	function openIndex(i) {
+		if (!onGoing && next != i) {
+			onGoing = true;
+			next = i
+			outClass = current > i ? 'rotateCubeBottomOut top' : 'rotateCubeTopOut top'
+			inClass = current > i ? 'rotateCubeBottomIn' : 'rotateCubeTopIn';
+			show()
+		}
+	}
+
+	function trans(direction) {
+		if (!onGoing) {
+			onGoing = true;
+			if (direction == 'up') {
+				next = current > 1 ? current - 1 : length;
+				outClass = 'rotateCubeBottomOut top';
+				inClass = 'rotateCubeBottomIn';
+			} else {
+				next = current < length ? current + 1 : 1;
+				outClass = 'rotateCubeTopOut top';
+				inClass = 'rotateCubeTopIn';
+			}
+			show();
+		}
+	}
+
+	function show() {
+		$('#cubeTransition>div:eq(' + (current - 1) + ')').addClass(outClass);
+		$('#cubeTransition>div:eq(' + (next - 1) + ')').addClass(inClass);
+		$('#bullets>li:eq(' + (current - 1) + ')').removeClass('active');
+		$('#bullets>li:eq(' + (next - 1) + ')').addClass('active');
+		$('#cubeTransition>div:eq(' + (next - 1) + ')').show();
+
+		setTimeout(function() {
+			$('#cubeTransition>div:eq(' + (current - 1) + ')').hide();
+		}, 500)
+
+		setTimeout(function() {
+			$('#cubeTransition>div:eq(' + (current - 1) + ')').removeClass(outClass);
+			$('#cubeTransition>div:eq(' + (next - 1) + ')').removeClass(inClass);
+			current = next;
+			onGoing = false;
+		}, 800)
+	}
+
+	$(document).ready(
+
+	function() {
+		$(document).mousewheel(function(e, delta) {
+			e.preventDefault();
+			if (delta < 0) {
+				trans('down')
+			} else {
+				trans('up')
+			}
+		});
+		$(document).keydown(function(e) {
+			if (e.keyCode == 38 || e && e.keyCode == 37) {
+				trans('up')
+			}
+			if (e.keyCode == 39 || e && e.keyCode == 40) {
+				trans('down')
+			}
+
+		});
+
+		$(document).swipe({
+			swipe: function(event, direction, distance, duration, fingerCount) {
+				if (direction == "up") {
+					trans('down')
+				} else if (direction == "down") {
+					trans('up')
+				}
+			}
+		});
+
+
+		$('#bullets>li').on('click', function() {
+			openIndex($(this).index() + 1);
+		});
+
+	});
+})(jQuery);
